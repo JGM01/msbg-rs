e58a21473b4a45e91cf71f6ca23261b7165d5711
JGM01 <jacobgermanamccray@gmail.com>
2026-08-06 21:11:00 -0400

    remove tests

--- src/lib.rs
+++ src/lib.rs
@@ -127,18 +127,3 @@ impl<T, const BSX_LOG2: u32> SparseGrid<T, BSX_LOG2> {
         }
     }
 }
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
